5f80875d6e47895d7374646bbda054ac1242c9e0
sava <savamedvedev@gmail.com>
2022-04-19 04:39:37 +0800

    first commit

---
+++ src/layout/GoodsList.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { GoodsItem } from "./GoodsItem";
+
+function GoodsList(props) {
+  const { goods = [] } = props;
+
+  if (!goods.length) {
+    return <h3>Nothing here</h3>;
+  }
+  return (
+    <div className="goods">
+      {goods.map((item) => (
+        <GoodsItem key={item.id} {...item} />
+      ))}
+    </div>
+  );
+}
+export { GoodsList };
